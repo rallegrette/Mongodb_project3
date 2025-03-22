4c5c1da318b7f725822adf17ffa1f20a3b0cb1ad
kiyazz <kiyazz123@gmail.com>
2025-03-22 01:41:58 -0400

    part A completed and verified

--- GetData.java
+++ GetData.java
@@ -46,11 +46,45 @@ public class GetData {
         
         try (Statement stmt = oracleConnection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
             // Your implementation goes here....
-            
-            
-            stmt.close();
+            ResultSet results = stmt.executeQuery("SELECT U.user_id, U.first_name, U.last_name, U.gender, U.year_of_birth, U.month_of_birth, U.day_of_birth, C.city_name, C.state_name, C.country_name, H.city_name, H.state_name, H.country_name FROM " + userTableName
+            + " U JOIN " + currentCityTableName + " CC ON CC.user_id=U.user_id JOIN " + cityTableName + " C ON C.city_id=CC.current_city_id"
+            + " JOIN " + hometownCityTableName + " HC ON HC.user_id=U.user_id JOIN " + cityTableName + " H ON H.city_id = HC.hometown_city_id");
+
+            while (results.next()) {
+                JSONObject object = new JSONObject();
+                int user_id = results.getInt(1);
+                object.put("user_id", user_id);
+                object.put("first_name", results.getString(2));
+                object.put("last_name", results.getString(3));
+                object.put("YOB", results.getInt(5));
+                object.put("MOB", results.getInt(6));
+                object.put("DOB", results.getInt(7));
+                object.put("gender", results.getString(4));
+                JSONObject current = new JSONObject();
+                current.put("city", results.getString(8));
+                current.put("state", results.getString(9));
+                current.put("country", results.getString(10));
+                JSONObject hometown = new JSONObject();
+                hometown.put("city", results.getString(11));
+                hometown.put("state", results.getString(12));
+                hometown.put("country", results.getString(13));
+                object.put("current", current);
+                object.put("hometown", hometown);
+                // get friends
+                Statement statement = oracleConnection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                ResultSet set = statement.executeQuery("SELECT * FROM " + friendsTableName +
+                        " WHERE user1_id = " + user_id + " AND user1_id<user2_id");
+                JSONArray friends = new JSONArray();
+                while (set.next()) {
+                    friends.put(set.getInt(2));
+                }
+                set.close();
+                statement.close();
+                object.put("friends", friends);
+                users_info.put(object);
+            }
         } catch (SQLException e) {
-            System.err.println(e.getMessage());
+            e.printStackTrace();
         }
 
         return users_info;
--- Main.java
+++ Main.java
@@ -9,8 +9,8 @@ import org.json.JSONArray;
 public class Main {
 
     static String dataType = "PUBLIC";
-    static String oracleUserName = "uniqname"; // replace with your uniqname
-    static String password = "password"; // replace with your Oracle password (default: eecsclass)
+    static String oracleUserName = "kiyazz"; // replace with your uniqname
+    static String password = "Kabotar17!!!"; // replace with your Oracle password (default: eecsclass)
 
     public static void main(String[] args) {
 
